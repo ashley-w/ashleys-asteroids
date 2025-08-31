5cd458a7b44b8524d9b8a7ccf63e8d6f584b4ff5
Ashley Washington <shlywshngtn@gmail.com>
2025-08-31 17:15:16 +0200

    Add player movement with W/S keys
    Implement move() method that moves player forward/backward based on rotation direction. Added PLAYER_SPEED constant for movement velocity.

--- player.py
+++ player.py
@@ -1,6 +1,6 @@
 import pygame
 from circleshape import CircleShape
-from constants import PLAYER_RADIUS, PLAYER_TURN_SPEED
+from constants import PLAYER_RADIUS, PLAYER_TURN_SPEED, PLAYER_SPEED
 
 # Player class that represents the player's ship
 class Player(CircleShape):
@@ -27,6 +27,10 @@ class Player(CircleShape):
         points = self.triangle()
         pygame.draw.polygon(screen, "white", points, 2)
 
+    def move(self, dt):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        self.position += forward * PLAYER_SPEED * dt
+
     def update(self, dt):
         keys = pygame.key.get_pressed()
 
@@ -34,3 +38,7 @@ class Player(CircleShape):
             self.rotate(-dt)
         if keys[pygame.K_d]:
             self.rotate(dt)
+        if keys[pygame.K_w]:
+            self.move(dt)
+        if keys[pygame.K_s]:
+            self.move(-dt)
